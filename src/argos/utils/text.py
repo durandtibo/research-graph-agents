584a54c8e2c6d5480df305a096ff2bad7d0b7e23
Thibaut Durand <durand.tibo@gmail.com>
2026-03-24 10:13:25 -0700

    Add `remove_empty_lines` (#29)

---
+++ src/argos/utils/text.py
@@ -0,0 +1,26 @@
+r"""Contain utility functions to process text."""
+
+from __future__ import annotations
+
+__all__ = ["remove_empty_lines"]
+
+
+def remove_empty_lines(text: str) -> str:
+    r"""Remove empty lines from a string.
+
+    Args:
+        text: The input string from which empty lines will be removed.
+
+    Returns:
+        A new string with all empty or whitespace-only lines removed.
+
+    Example:
+        ```pycon
+        >>> remove_empty_lines("Hello\\n\\nWorld\\n\\n\\nFoo")
+        'Hello\\nWorld\\nFoo'
+        >>> remove_empty_lines("\\n\\nOnly empty lines\\n\\n")
+        'Only empty lines'
+
+        ```
+    """
+    return "\n".join([line for line in text.splitlines() if line.strip()])

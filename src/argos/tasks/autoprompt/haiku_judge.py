51a2e8ee0a4ca6546af4353f661b3913db0a7c6e
Thibaut Durand <durand.tibo@gmail.com>
2026-04-02 10:53:40 -0700

    Add `create_graph` (#59)

--- src/argos/tasks/autoprompt/haiku_judge.py
+++ src/argos/tasks/autoprompt/haiku_judge.py
@@ -2,17 +2,52 @@ r"""Contain code to run the autoprompt on the haiku dataset."""
 
 from __future__ import annotations
 
-__all__ = ["prepare_dataset"]
+__all__ = ["create_graph", "prepare_dataset"]
 
 import logging
+from typing import TYPE_CHECKING
 
 import polars as pl
 from coola.utils.timing import timeblock
+from langchain.chat_models import init_chat_model
+from langgraph.constants import END, START
+from langgraph.graph.state import CompiledStateGraph, StateGraph
 
 from argos.datasets import generate_haiku_dataset
+from argos.nodes import HaikuJudgeState, make_haiku_judge_node
 from argos.utils.dataframe import summarize_boolean_columns
 
-logger = logging.getLogger(__name__)
+if TYPE_CHECKING:
+    from langchain_core.language_models import BaseChatModel
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def create_graph(model: str, judge_system_prompt: str) -> CompiledStateGraph:
+    r"""Create the graph of the haiku generator-judge.
+
+    Args:
+        model: The model of the haiku generator-judge.
+        judge_system_prompt: The prompt of the judge-system-prompt.
+
+    Returns:
+        The graph of the haiku generator-judge.
+    """
+    llm: BaseChatModel = init_chat_model(model=model, temperature=0, max_retries=9999)
+    model_version = getattr(llm, "model", getattr(llm, "model_name", "Unknown"))
+    logger.info(
+        f"class: {type(llm).__name__} | model: {model_version} | temperature: {llm.temperature}"
+    )
+
+    graph_builder = StateGraph(HaikuJudgeState)
+
+    graph_builder.add_node("judge", make_haiku_judge_node(llm, system_prompt=judge_system_prompt))
+
+    graph_builder.add_edge(START, "judge")
+    graph_builder.add_edge("judge", END)
+
+    # Compile the graph into a runnable app
+    return graph_builder.compile()
 
 
 def prepare_dataset() -> pl.DataFrame:

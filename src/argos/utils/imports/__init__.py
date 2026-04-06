4c62fcf75360146cbbfbb33017d736cc79af1e1b
Thibaut Durand <durand.tibo@gmail.com>
2026-04-06 09:15:04 -0700

    Add `is_rich_available` (#69)

--- src/argos/utils/imports/__init__.py
+++ src/argos/utils/imports/__init__.py
@@ -4,9 +4,13 @@ from __future__ import annotations
 
 __all__ = [
     "check_colorlog",
+    "check_rich",
     "colorlog_available",
     "is_colorlog_available",
+    "is_rich_available",
     "raise_colorlog_missing_error",
+    "raise_rich_missing_error",
+    "rich_available",
 ]
 
 from argos.utils.imports.colorlog import (
@@ -14,4 +18,10 @@ from argos.utils.imports.colorlog import (
     colorlog_available,
     is_colorlog_available,
     raise_colorlog_missing_error,
+)
+from argos.utils.imports.rich import (
+    check_rich,
+    is_rich_available,
+    raise_rich_missing_error,
+    rich_available,
 )

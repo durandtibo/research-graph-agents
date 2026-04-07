ca622243b1348af11cc81b544fd9f7e6d1bd7da1
Thibaut Durand <durand.tibo@gmail.com>
2026-04-07 09:51:37 -0700

    Add `BaseHistory`

--- tests/unit/tasks/autoprompt/test_history.py
+++ tests/unit/tasks/autoprompt/test_history.py
@@ -6,53 +6,57 @@ import pytest
 from feu.utils.io import save_json
 from iden.io import load_json
 
-from argos.tasks.autoprompt.config import ExperimentConfig
-from argos.tasks.autoprompt.history import append_to_history, create_history
+from argos.tasks.autoprompt.history import (
+    JsonHistory,
+)
 
 if TYPE_CHECKING:
     from pathlib import Path
 
 
 @pytest.fixture
-def config(tmp_path: Path) -> ExperimentConfig:
-    return ExperimentConfig(
-        judge_model="llama",
-        judge_system_prompt="my prompt",
-        path_experiment=tmp_path.joinpath("data"),
-    )
+def json_path(tmp_path: Path) -> Path:
+    path = tmp_path.joinpath("data").joinpath("history.json")
+    save_json([{"key": "value"}], path)
+    return path
 
 
-#######################################
-#     Tests for append_to_history     #
-#######################################
+#################################
+#     Tests for JsonHistory     #
+#################################
 
 
-def test_append_to_history_empty(config: ExperimentConfig) -> None:
-    append_to_history(config, {"hello": "world"})
-    assert config.path_history.is_file()
-    assert load_json(config.path_history) == [{"hello": "world"}]
+def test_json_history_file_does_not_exist(tmp_path: Path) -> None:
+    history = JsonHistory(tmp_path.joinpath("data"))
+    assert history.path.is_file()
+    assert load_json(history.path) == []
 
 
-def test_append_to_history_empty_not_empty(config: ExperimentConfig) -> None:
-    save_json([{"key": "value"}], config.path_history)
-    append_to_history(config, {"hello": "world"})
-    assert config.path_history.is_file()
-    assert load_json(config.path_history) == [{"key": "value"}, {"hello": "world"}]
+def test_json_history_file_already_exists(json_path: Path) -> None:
+    history = JsonHistory(json_path)
+    assert history.path.is_file()
+    assert load_json(history.path) == [{"key": "value"}]
 
 
-####################################
-#     Tests for create_history     #
-####################################
+def test_json_history_file_append_empty(tmp_path: Path) -> None:
+    history = JsonHistory(tmp_path.joinpath("data").joinpath("history.json"))
+    history.append({"hello": "world"})
+    assert history.path.is_file()
+    assert load_json(history.path) == [{"hello": "world"}]
 
 
-def test_create_history_does_not_exist(config: ExperimentConfig) -> None:
-    create_history(config)
-    assert config.path_history.is_file()
-    assert load_json(config.path_history) == []
+def test_json_history_file_append_not_empty(json_path: Path) -> None:
+    history = JsonHistory(json_path)
+    history.append({"hello": "world"})
+    assert history.path.is_file()
+    assert load_json(history.path) == [{"key": "value"}, {"hello": "world"}]
 
 
-def test_create_history_exists(config: ExperimentConfig) -> None:
-    save_json([{"key": "value"}], config.path_history)
-    create_history(config)
-    assert config.path_history.is_file()
-    assert load_json(config.path_history) == [{"key": "value"}]
+def test_json_history_file_get_values_empty(tmp_path: Path) -> None:
+    history = JsonHistory(tmp_path.joinpath("data").joinpath("history.json"))
+    assert history.get_values() == []
+
+
+def test_json_history_file_get_values_not_empty(json_path: Path) -> None:
+    history = JsonHistory(json_path)
+    assert history.get_values() == [{"key": "value"}]

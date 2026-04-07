a29c5ecf70d41c77974f802f9dd95344757b94a7
Thibaut Durand <durand.tibo@gmail.com>
2026-04-07 10:26:53 -0700

    Add `LlmConfig` (#94)

--- src/argos/tasks/autoprompt/config.py
+++ src/argos/tasks/autoprompt/config.py
@@ -2,13 +2,26 @@ r"""Contain the experiment configuration."""
 
 from __future__ import annotations
 
-__all__ = ["ExperimentConfig"]
+__all__ = ["ExperimentConfig", "LlmConfig"]
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from pathlib import Path
+
+
+@dataclass
+class LlmConfig:
+    r"""A generic LLM configuration."""
+
+    model: str
+    system_prompt: str
+    batch_size: int = 1
+    # Optional keyword arguments that will be passed to `init_chat_model`
+    max_retries: int = 9999
+    temperature: float = 0.0
+    init_kwargs: dict[str, Any] | None = None
 
 
 @dataclass
@@ -22,6 +35,9 @@ class ExperimentConfig:
     iteration: int = 0
     # path_history: Path
     # prompt_model: str
+    # judge: LlmConfig
+    # prompt: LlmConfig
+    # analyzer: LlmConfig
 
     @property
     def path_history(self) -> Path:

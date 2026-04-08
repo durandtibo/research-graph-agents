5eacb8534c7979ecfa2c07c81cf8df974c35702e
Thibaut Durand <durand.tibo@gmail.com>
2026-04-08 10:03:05 -0700

    Add new `create_judge_graph` (#109)

---
+++ src/argos/tasks/autoprompt/judge2.py
@@ -0,0 +1,55 @@
+r"""Contain code to run the autoprompt on the haiku dataset."""
+
+from __future__ import annotations
+
+__all__ = ["create_judge_graph"]
+
+import logging
+from typing import TYPE_CHECKING
+
+from langchain.chat_models import init_chat_model
+from langgraph.constants import END, START
+from langgraph.graph.state import CompiledStateGraph, StateGraph
+
+from argos.nodes import HaikuJudgeState, make_haiku_judge_node
+
+if TYPE_CHECKING:
+    from langchain_core.language_models import BaseChatModel
+
+    from argos.tasks.autoprompt.config import LlmConfig
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def create_judge_graph(config: LlmConfig) -> CompiledStateGraph:
+    r"""Create the graph of the haiku judge.
+
+    Args:
+        config: The LLM config.
+
+    Returns:
+        The graph of the haiku judge.
+    """
+    logger.info("Creating judge graph...")
+    if config.temperature > 0:
+        logger.warning("It is recommended to set temperature to 0 to have a deterministic judge")
+    init_kwargs = config.init_kwargs or {}
+    llm: BaseChatModel = init_chat_model(
+        model=config.model,
+        temperature=config.temperature,
+        max_retries=config.max_retries,
+        **init_kwargs,
+    )
+    model_version = getattr(llm, "model", getattr(llm, "model_name", "Unknown"))
+    logger.info(
+        f"class: {type(llm).__name__} | model: {model_version} | temperature: {llm.temperature}"
+    )
+
+    graph_builder = StateGraph(HaikuJudgeState)
+
+    graph_builder.add_node("judge", make_haiku_judge_node(llm, system_prompt=config.system_prompt))
+
+    graph_builder.add_edge(START, "judge")
+    graph_builder.add_edge("judge", END)
+
+    return graph_builder.compile()

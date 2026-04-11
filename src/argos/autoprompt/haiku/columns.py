e5a772ea9b68a467034fed9ee167695719d68844
Thibaut Durand <durand.tibo@gmail.com>
2026-04-11 15:40:35 -0700

    Add column names

---
+++ src/argos/autoprompt/haiku/columns.py
@@ -0,0 +1,24 @@
+r"""Contain column names."""
+
+__all__ = [
+    "HAIKU",
+    "OVERALL_PREDICTION",
+    "OVERALL_TARGET",
+    "STRUCTURE_PREDICTION",
+    "STRUCTURE_TARGET",
+    "TOPIC",
+    "TOPIC_PREDICTION",
+    "TOPIC_TARGET",
+]
+
+HAIKU = "haiku"
+TOPIC = "topic"
+
+OVERALL_PREDICTION = "overall_prediction"
+OVERALL_TARGET = "overall_target"
+
+STRUCTURE_PREDICTION = "structure_prediction"
+STRUCTURE_TARGET = "structure_target"
+
+TOPIC_PREDICTION = "topic_prediction"
+TOPIC_TARGET = "topic_target"

2f83f5af599baa30a8a1085a9c7cdc07b9f673ec
Thibaut Durand <durand.tibo@gmail.com>
2026-04-11 16:15:19 -0700

    Update `evaluate_classification_metrics`

--- src/argos/autoprompt/haiku/evaluation.py
+++ src/argos/autoprompt/haiku/evaluation.py
@@ -2,11 +2,12 @@ r"""Contain the code to evaluate the performances."""
 
 from __future__ import annotations
 
-__all__ = ["evaluate_metrics"]
+__all__ = ["evaluate_classification_metrics"]
 
 import logging
 from typing import TYPE_CHECKING
 
+from argos.autoprompt.haiku import columns
 from argos.metrics import (
     BinaryClassificationResults,
     compute_binary_classification_metrics,
@@ -18,7 +19,16 @@ if TYPE_CHECKING:
 logger: logging.Logger = logging.getLogger(__name__)
 
 
-def evaluate_metrics(predictions: pl.DataFrame) -> dict[str, BinaryClassificationResults]:
+def evaluate_classification_metrics(
+    predictions: pl.DataFrame,
+    *,
+    overall_prediction_col: str = columns.OVERALL_PREDICTION,
+    overall_target_col: str = columns.OVERALL_TARGET,
+    structure_prediction_col: str = columns.STRUCTURE_PREDICTION,
+    structure_target_col: str = columns.STRUCTURE_TARGET,
+    topic_prediction_col: str = columns.TOPIC_PREDICTION,
+    topic_target_col: str = columns.TOPIC_TARGET,
+) -> dict[str, BinaryClassificationResults]:
     r"""Evaluate the metrics of the haiku judge.
 
     Computes binary classification metrics for three prediction autoprompt:
@@ -42,17 +52,17 @@ def evaluate_metrics(predictions: pl.DataFrame) -> dict[str, BinaryClassificatio
     """
     logger.info("Evaluating metrics...")
     overall = compute_binary_classification_metrics(
-        predictions, target_col="target", prediction_col="passed"
+        predictions, target_col=overall_target_col, prediction_col=overall_prediction_col
     )
     logger.info(f"overall\n{overall.to_str()}")
 
     structure = compute_binary_classification_metrics(
-        predictions, target_col="structure_target", prediction_col="structure_passed"
+        predictions, target_col=structure_target_col, prediction_col=structure_prediction_col
     )
     logger.info(f"structure\n{structure.to_str()}")
 
     topic = compute_binary_classification_metrics(
-        predictions, target_col="topic_target", prediction_col="topic_passed"
+        predictions, target_col=topic_target_col, prediction_col=topic_prediction_col
     )
     logger.info(f"topic\n{topic.to_str()}")
     return {"overall": overall, "structure": structure, "topic": topic}

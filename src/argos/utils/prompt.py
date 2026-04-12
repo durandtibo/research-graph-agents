9974e4678ed9cb028bdd4e89e59d60a81e8b6a67
Thibaut Durand <durand.tibo@gmail.com>
2026-04-12 15:40:53 -0700

    Add `check_non_empty_prompt`

---
+++ src/argos/utils/prompt.py
@@ -0,0 +1,17 @@
+r"""Contain utility functions for prompts."""
+
+from __future__ import annotations
+
+__all__ = ["check_non_empty_prompt"]
+
+
+def check_non_empty_prompt(prompt: str, name: str = "prompt") -> None:
+    r"""Check if the input prompt is not empty.
+
+    Args:
+        prompt: The input prompt to check.
+        name: The name of the input prompt to show in the error message if empty.
+    """
+    if not prompt.strip():
+        msg = f"{name} must be a non-empty string"
+        raise ValueError(msg)
---
+++ tests/unit/utils/test_prompt.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import pytest
+
+from argos.utils.prompt import check_non_empty_prompt
+
+############################################
+#     Tests for check_non_empty_prompt     #
+############################################
+
+
+@pytest.mark.parametrize("prompt", ["a", " meow ", "meow\nmeow\nmeow"])
+def test_check_non_empty_prompt_non_empty(prompt: str) -> None:
+    check_non_empty_prompt(prompt)
+
+
+@pytest.mark.parametrize("prompt", ["", " ", "\n\n"])
+def test_check_non_empty_prompt_empty(prompt: str) -> None:
+    with pytest.raises(ValueError, match="prompt must be a non-empty string"):
+        check_non_empty_prompt(prompt)

748b57a959c024d34f8fa90ce91161c6ca014d85
Thibaut Durand <durand.tibo@gmail.com>
2026-04-21 08:59:19 -0700

    Add `Agent`

--- src/argos/meta_agent/agent/__init__.py
+++ src/argos/meta_agent/agent/__init__.py
@@ -1,7 +1,8 @@
 r"""Contain code to manage agents."""
 
-__all__ = ["AgentConfig", "BaseAgent", "BaseAgentFactory"]
+__all__ = ["Agent", "AgentConfig", "BaseAgent", "BaseAgentFactory"]
 
 from argos.meta_agent.agent.base import BaseAgent
 from argos.meta_agent.agent.config import AgentConfig
 from argos.meta_agent.agent.factory import BaseAgentFactory
+from argos.meta_agent.agent.vanilla import Agent
---
+++ src/argos/meta_agent/agent/vanilla.py
@@ -0,0 +1,30 @@
+r"""Implement a simple agent."""
+
+from __future__ import annotations
+
+__all__ = ["Agent"]
+
+
+from typing import TYPE_CHECKING, TypeVar
+
+from argos.meta_agent.agent.base import BaseAgent
+
+if TYPE_CHECKING:
+    from langchain_core.runnables import Runnable
+
+InputT = TypeVar("InputT")
+OutputT = TypeVar("OutputT")
+
+
+class Agent(BaseAgent[InputT, OutputT]):
+    r"""Define a simple agent that is a wrapper around a runnable object.
+
+    Args:
+        model: An instance of a runnable object used as model for the agent.
+    """
+
+    def __init__(self, model: Runnable[InputT, OutputT]) -> None:
+        self._model = model
+
+    def predict(self, inputs: list[InputT]) -> list[OutputT]:
+        return self._model.batch(inputs)

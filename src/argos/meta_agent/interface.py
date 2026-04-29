418b3cd45c8e44d10539db58c9218f7846b4a13b
Thibaut Durand <durand.tibo@gmail.com>
2026-04-29 15:37:51 -0700

    Update `PredictionResult` (#236)

--- src/argos/meta_agent/interface.py
+++ src/argos/meta_agent/interface.py
@@ -5,7 +5,10 @@ from __future__ import annotations
 __all__ = ["Benchmark", "BenchmarkExample", "PredictionRecord", "PredictionResult"]
 
 from dataclasses import dataclass
-from typing import Any, Generic, TypeVar
+from typing import TYPE_CHECKING, Any, Generic, TypeVar
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
 
 InputT = TypeVar("InputT")
 TargetT = TypeVar("TargetT")
@@ -47,3 +50,30 @@ class PredictionResult(Generic[PredictionT]):
     r"""Define the prediction result class."""
 
     records: list[PredictionRecord[PredictionT]]
+
+    @classmethod
+    def from_predictions(
+        cls, example_ids: Sequence[str], predictions: Sequence[PredictionT]
+    ) -> PredictionResult[PredictionT]:
+        r"""Create a prediction result from a list of example IDs and
+        predictions.
+
+        example_ids and predictions must be aligned i.e. the n-th
+        example ID should match the n-th prediction.
+
+        Args:
+            example_ids: A list of example IDs
+            predictions: A list of predictions
+
+        Returns:
+            The prediction result.
+        """
+        if len(example_ids) != len(predictions):
+            msg = "example_ids and predictions must have the same length"
+            raise ValueError(msg)
+        return PredictionResult(
+            [
+                PredictionRecord(example_id=example_id, prediction=prediction)
+                for example_id, prediction in zip(example_ids, predictions)
+            ]
+        )

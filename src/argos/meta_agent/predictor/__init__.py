e434e2579709fd0014bad9f5988c5436a469865e
Thibaut Durand <durand.tibo@gmail.com>
2026-04-30 04:53:36 -0700

    Add `BasePredictor` and `BatchPredictor` (#238)

---
+++ src/argos/meta_agent/predictor/__init__.py
@@ -0,0 +1,8 @@
+r"""Contain the predictors."""
+
+from __future__ import annotations
+
+__all__ = ["BasePredictor", "BatchPredictor"]
+
+from argos.meta_agent.predictor.base import BasePredictor
+from argos.meta_agent.predictor.batch import BatchPredictor

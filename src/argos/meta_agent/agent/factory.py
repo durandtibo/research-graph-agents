2d9bbb4e1fb6dfa2134f35c0622862cda6fee7b2
Thibaut Durand <durand.tibo@gmail.com>
2026-04-30 04:56:38 -0700

    Fix type hints (#239)

--- src/argos/meta_agent/agent/factory.py
+++ src/argos/meta_agent/agent/factory.py
@@ -6,18 +6,22 @@ from __future__ import annotations
 __all__ = ["BaseAgentFactory"]
 
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Generic, TypeVar
 
 if TYPE_CHECKING:
     from argos.meta_agent.agent.base import BaseAgent
     from argos.meta_agent.agent.config import AgentConfig
 
 
-class BaseAgentFactory(ABC):
+InputT = TypeVar("InputT")
+OutputT = TypeVar("OutputT")
+
+
+class BaseAgentFactory(ABC, Generic[InputT, OutputT]):
     r"""Define the base class to implement an agent factory."""
 
     @abstractmethod
-    def create(self, config: AgentConfig) -> BaseAgent:
+    def create(self, config: AgentConfig) -> BaseAgent[InputT, OutputT]:
         r"""Instantiate an agent from its configuration.
 
         Args:
--- src/argos/meta_agent/state.py
+++ src/argos/meta_agent/state.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __all__ = ["MetaAgentState"]
 
-from typing import TYPE_CHECKING, TypedDict
+from typing import TYPE_CHECKING, Any, TypedDict
 
 if TYPE_CHECKING:
     from argos.meta_agent.agent.config import AgentConfig
@@ -14,7 +14,7 @@ class MetaAgentState(TypedDict):
     r"""Define the meta-agent state."""
 
     config: AgentConfig
-    metrics: dict
-    diagnostic: dict
-    history: list[dict]
+    metrics: dict[Any, Any]
+    diagnostic: dict[Any, Any]
+    history: list[dict[Any, Any]]
     iteration: int

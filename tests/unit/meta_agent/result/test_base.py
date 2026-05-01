147469a5a5cf0d6b2745622c04cc98725a2e816a
Thibaut Durand <durand.tibo@gmail.com>
2026-05-01 13:48:21 -0700

    Add missing tests

---
+++ tests/unit/meta_agent/result/test_base.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from coola.equality.tester import get_default_registry
+
+from argos.meta_agent.result import BaseResult
+
+
+def test_equality_tester_registry_has_equality_tester() -> None:
+    assert get_default_registry().has_equality_tester(BaseResult)

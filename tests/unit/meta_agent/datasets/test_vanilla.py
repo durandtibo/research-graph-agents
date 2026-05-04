ebfc568f8d2c0b42fb16d2a3bcf3808766940742
Thibaut Durand <durand.tibo@gmail.com>
2026-05-03 19:46:56 -0700

    Add `Dataset`

---
+++ tests/unit/meta_agent/datasets/test_vanilla.py
@@ -0,0 +1,114 @@
+r"""Unit tests for PredictionResult.from_predictions."""
+
+import polars as pl
+import pytest
+from polars.testing import assert_frame_equal
+
+from argos.meta_agent.datasets import Dataset
+from argos.meta_agent.examples import BaseExample, Example
+
+
+@pytest.fixture
+def dataset() -> Dataset:
+    return Dataset(
+        {
+            "id1": Example(id="id1", input="input1", target="target1", metadata={"tag": "tag1"}),
+            "id2": Example(id="id2", input="input2", target="target2", metadata={"tag": "tag2"}),
+            "id3": Example(id="id3", input="input3", target="target3", metadata={"tag": "tag3"}),
+            "id4": Example(id="id4", input="input4", target="target4", metadata={"tag": "tag4"}),
+            "id5": Example(id="id5", input="input5", target="target5", metadata={"tag": "tag5"}),
+        }
+    )
+
+
+@pytest.fixture
+def dataset_with_metadata(dataset: Dataset) -> Dataset:
+    dataset.metadata = {"tag": "meow"}
+    return dataset
+
+
+@pytest.fixture
+def dataframe() -> pl.DataFrame:
+    return pl.DataFrame(
+        {
+            "id": ["id1", "id2", "id3", "id4", "id5"],
+            "input": ["input1", "input2", "input3", "input4", "input5"],
+            "target": ["target1", "target2", "target3", "target4", "target5"],
+            "metadata": [
+                {"tag": "tag1"},
+                {"tag": "tag2"},
+                {"tag": "tag3"},
+                {"tag": "tag4"},
+                {"tag": "tag5"},
+            ],
+        },
+        schema=pl.Schema(
+            {
+                "id": pl.String,
+                "input": pl.String,
+                "target": pl.String,
+                "metadata": pl.Struct({"tag": pl.String}),
+            },
+        ),
+    )
+
+
+@pytest.fixture
+def examples() -> list[BaseExample]:
+    return [
+        Example(id="id1", input="input1", target="target1", metadata={"tag": "tag1"}),
+        Example(id="id2", input="input2", target="target2", metadata={"tag": "tag2"}),
+        Example(id="id3", input="input3", target="target3", metadata={"tag": "tag3"}),
+        Example(id="id4", input="input4", target="target4", metadata={"tag": "tag4"}),
+        Example(id="id5", input="input5", target="target5", metadata={"tag": "tag5"}),
+    ]
+
+
+#############################
+#     Tests for Dataset     #
+#############################
+
+
+def test_dataset_to_dataframe(dataset: Dataset, dataframe: pl.DataFrame) -> None:
+    assert_frame_equal(dataset.to_dataframe(), dataframe)
+
+
+def test_dataset_to_dataframe_empty() -> None:
+    assert_frame_equal(
+        Dataset({}).to_dataframe(),
+        pl.DataFrame({}),
+    )
+
+
+def test_dataset_from_examples(dataset: Dataset, examples: list[BaseExample]) -> None:
+    assert dataset == Dataset.from_examples(examples)
+
+
+def test_dataset_from_examples_with_metadata(
+    dataset_with_metadata: Dataset, examples: list[BaseExample]
+) -> None:
+    assert dataset_with_metadata == Dataset.from_examples(
+        examples,
+        metadata={"tag": "meow"},
+    )
+
+
+def test_dataset_from_examples_empty() -> None:
+    assert Dataset.from_examples([]) == Dataset({})
+
+
+def test_dataset_from_examples_empty_with_metadata() -> None:
+    assert Dataset.from_examples([], metadata={"tag": "meow"}) == Dataset(
+        {}, metadata={"tag": "meow"}
+    )
+
+
+def test_dataset_from_examples_with_duplicated_example_ids() -> None:
+    with pytest.raises(ValueError, match="Some example IDs are duplicated"):
+        Dataset.from_examples(
+            [
+                Example(id="id1", input="input1", target="target1", metadata={"tag": "tag1"}),
+                Example(id="id2", input="input2", target="target2", metadata={"tag": "tag2"}),
+                Example(id="id2", input="input3", target="target3", metadata={"tag": "tag3"}),
+            ]
+        )

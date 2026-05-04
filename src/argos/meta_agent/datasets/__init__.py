5498642fcd7178ff498b560cf20bd1b5cc90baad
Thibaut Durand <durand.tibo@gmail.com>
2026-05-03 18:57:33 -0700

    Add `BaseDataset` (#275)

--- src/argos/meta_agent/datasets/__init__.py
+++ src/argos/meta_agent/datasets/__init__.py
@@ -1,1 +1,7 @@
 r"""Contain abstractions and implementations for datasets."""
+
+from __future__ import annotations
+
+__all__ = ["BaseDataset"]
+
+from argos.meta_agent.datasets.base import BaseDataset

d5810d73bde2d3b25f461ed19d5cb7880b6fd0b4
Thibaut Durand <durand.tibo@gmail.com>
2026-05-03 20:28:03 -0700

    Fix format (#281)

--- src/argos/meta_agent/results/mapping.py
+++ src/argos/meta_agent/results/mapping.py
@@ -74,9 +74,7 @@ class ResultDict(BaseResult):
         Example:
             ```pycon
             >>> from argos.meta_agent.results import Result, ResultDict
-            >>> result = ResultDict(
-            ...     {"train": Result({"loss": 0.5}), "val": Result({"loss": 0.3})}
-            ... )
+            >>> result = ResultDict({"train": Result({"loss": 0.5}), "val": Result({"loss": 0.3})})
             >>> result.to_flat_dict()
             {'train.loss': 0.5, 'val.loss': 0.3}
 

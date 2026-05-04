8b44985360984a966565858c83e3c7bf01b63752
Thibaut Durand <durand.tibo@gmail.com>
2026-05-03 18:46:36 -0700

    Add `BaseExample` and `Example` (#274)

--- src/argos/meta_agent/examples/__init__.py
+++ src/argos/meta_agent/examples/__init__.py
@@ -1,1 +1,8 @@
 r"""Contain abstractions and implementations for examples."""
+
+from __future__ import annotations
+
+__all__ = ["BaseExample", "Example"]
+
+from argos.meta_agent.examples.base import BaseExample
+from argos.meta_agent.examples.vanilla import Example

8b0c69fa6ecb0d3df2908f424bd412e76dc41739
Thibaut Durand <durand.tibo@gmail.com>
2026-05-04 13:32:55 -0700

    Add `BaseAnalyzer`

---
+++ src/argos/meta_agent/analyzers/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain abstractions and implementations for analyzers."""
+
+from __future__ import annotations
+
+__all__ = ["BaseAnalyzer"]
+
+from argos.meta_agent.analyzers.base import BaseAnalyzer

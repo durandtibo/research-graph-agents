7f502955473ad3588f034d06931096d4627d9604
Thibaut Durand <durand.tibo@gmail.com>
2026-05-05 10:22:13 -0700

    Add `JsonExportAnalyzer`

--- src/argos/meta_agent/analyzers/__init__.py
+++ src/argos/meta_agent/analyzers/__init__.py
@@ -2,7 +2,8 @@ r"""Contain abstractions and implementations for analyzers."""
 
 from __future__ import annotations
 
-__all__ = ["BaseAnalyzer", "NoOpAnalyzer"]
+__all__ = ["BaseAnalyzer", "JsonExportAnalyzer", "NoOpAnalyzer"]
 
 from argos.meta_agent.analyzers.base import BaseAnalyzer
+from argos.meta_agent.analyzers.export import JsonExportAnalyzer
 from argos.meta_agent.analyzers.noop import NoOpAnalyzer

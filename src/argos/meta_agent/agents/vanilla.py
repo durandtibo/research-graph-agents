aa005ad345b52091fb29e7397fc2c5d83a821d67
Thibaut Durand <durand.tibo@gmail.com>
2026-05-06 07:25:08 -0700

    Update `Agent`

--- src/argos/meta_agent/agents/vanilla.py
+++ src/argos/meta_agent/agents/vanilla.py
@@ -7,6 +7,8 @@ __all__ = ["Agent"]
 
 from typing import TYPE_CHECKING, TypeVar
 
+from coola.utils.format import repr_indent, repr_mapping, str_indent, str_mapping
+
 from argos.meta_agent.agents.base import BaseAgent
 
 if TYPE_CHECKING:
@@ -20,21 +22,33 @@ class Agent(BaseAgent[InputT, OutputT]):
     r"""Define a simple agent that is a wrapper around a runnable object.
 
     Args:
-        model: An instance of a runnable object used as model for the agent.
+        runnable: An instance of a runnable object.
 
     Example:
         ```pycon
         >>> from langchain_core.runnables import RunnableLambda
         >>> from argos.meta_agent.agents import Agent
         >>> agent = Agent(RunnableLambda(str.upper))
+        >>> agent
+        Agent(
+          (runnable): RunnableLambda(upper)
+        )
         >>> agent.predict(["hello", "world"])
         ['HELLO', 'WORLD']
 
         ```
     """
 
-    def __init__(self, model: Runnable[InputT, OutputT]) -> None:
-        self._model = model
+    def __init__(self, runnable: Runnable[InputT, OutputT]) -> None:
+        self._runnable = runnable
+
+    def __repr__(self) -> str:
+        args = repr_indent(repr_mapping({"runnable": self._runnable}))
+        return f"{self.__class__.__qualname__}(\n  {args}\n)"
+
+    def __str__(self) -> str:
+        args = str_indent(str_mapping({"runnable": self._runnable}))
+        return f"{self.__class__.__qualname__}(\n  {args}\n)"
 
     def predict(self, inputs: list[InputT], config: RunnableConfig | None = None) -> list[OutputT]:
-        return self._model.batch(inputs=inputs, config=config)
+        return self._runnable.batch(inputs=inputs, config=config)

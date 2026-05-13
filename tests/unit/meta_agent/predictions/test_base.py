74fffb1b88ee245d4dfb06e9749d1c80b29e30ab
Thibaut Durand <durand.tibo@gmail.com>
2026-05-13 12:03:48 -0700

    Remove `predictions` (#413)

--- tests/unit/meta_agent/predictions/test_base.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from coola.equality.tester import get_default_registry
-
-from argos.meta_agent.predictions import BasePrediction
-
-
-def test_has_equality_tester_for_base_prediction() -> None:
-    assert get_default_registry().has_equality_tester(BasePrediction)

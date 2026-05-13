d308d2d950b3c31a1526482a821a7a0ad08208bd
Thibaut Durand <durand.tibo@gmail.com>
2026-05-13 12:01:17 -0700

    Remove `examples`

--- tests/unit/meta_agent/examples/test_base.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from coola.equality.tester import get_default_registry
-
-from argos.meta_agent.examples import BaseExample
-
-
-def test_has_equality_tester_for_base_example() -> None:
-    assert get_default_registry().has_equality_tester(BaseExample)
